d2913a48b3f1dfe552f25ce3bee8ff7ab8269a4e
Harsh K <hersh.k17@gmail.com>
2022-03-11 16:22:27 -0500

    lab4 video added

--- src/labs/Lab4.js
+++ src/labs/Lab4.js
@@ -1,11 +1,31 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import Footer from "../Footer";
 
 export default function Lab4() {
   return (
     <div>
-      This is the content for Lab 4
-      <Footer datetime="28/01/2022, 07:17:52 PM"/>
+      <div className="video-grid">
+        <div className="video-player">
+          <ReactPlayer
+            height="500px"
+            width="auto"
+            url="/assets/video/penguin.MP4"
+            controls={true}
+          />
+        </div>
+        <div className="video-desc">
+          <h3>Discussion</h3>
+          <p>
+            Text goes here
+          </p>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer datetime="11/03/2022, 04:01:29 PM" />
     </div>
   );
 }
