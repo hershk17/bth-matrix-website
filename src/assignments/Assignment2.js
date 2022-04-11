50f909f5ee6a61db5a4b5777bf3ad312ffb0bf2e
Harsh K <hersh.k17@gmail.com>
2022-04-10 21:02:45 -0400

    added a2 video

--- src/assignments/Assignment2.js
+++ src/assignments/Assignment2.js
@@ -1,11 +1,36 @@
 import React from "react";
 import Footer from "../Footer";
 
+import ReactPlayer from "react-player";
+
 export default function Assignment2() {
   return (
     <div>
-      This is the content for Assignment 2
-      <Footer datetime="28/01/2022, 07:17:52 PM" />
+      <h1>Assignment 2</h1>
+      <p>
+        I declare that this is wholly my own work in accordance with Seneca
+        Academic Policy. No part of this work has been copied manually or
+        electronically from any other source (including web sites) or
+        distributed to other students.
+        <br />
+        <br />
+        Name <b>Harsh K</b> <br />
+        Student ID <b>127318194</b>
+      </p>
+      <br />
+      <ReactPlayer url="https://youtu.be/MgaF6KuCLmo" controls={true} />
+      <br />
+      <br />
+      <h3>Script</h3>
+      <p>Script goes here</p>
+      <br />
+      <h3>Reflection</h3>
+      <p>Reflection goes here</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer datetime="10/04/2022, 08:57:52 PM" />
     </div>
   );
 }
